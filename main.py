8479699f13c7eeae43b6f4b657570f9bbfdd4b64
charlychiu <charlychiu@gmail.com>
2018-11-20 21:39:04 +0800

    Modify testing data

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ sentence_list = []
 with open('cprob_brown_2gram.pickle', 'rb') as file:
     cprob_brown_2gram = pickle.load(file)
 
-
 # print(cprob_brown_2gram["我"].prob("們"))
 
 # freq_brown_1gram = nltk.FreqDist(sentence_list)
@@ -63,9 +62,18 @@ with open('freq_brown_1gram.pickle', 'rb') as file:
 with open('len_brown.pickle', 'rb') as file:
     len_brown = pickle.load(file)
 
+
 def unigram_prob(word):
     return freq_brown_1gram[word] / len_brown
 
-prob_sentence = unigram_prob("民") * cprob_brown_2gram["民"].prob("主") * cprob_brown_2gram["主"].prob("國")* cprob_brown_2gram["國"].prob("家")
 
-print(prob_sentence) # 0.28532904004829945
+testing_data = ['川普任職於白宮', '一種基本人權', '人人有保持意見不受干預之權利', '我思故我在', '柏拉圖式戀愛', '奇美博物館主場館', '供應鏈上製造了混亂', '加州史丹佛大學理學博士生',
+                '沒想到開季又是內角觸身', '雲端基礎設施服務市場']
+for data in testing_data:
+    prob_sentence1 = unigram_prob(data[0])
+    for i in range(1, len(data)):
+        prob_sentence1 *= cprob_brown_2gram[data[i - 1]].prob(data[i])
+    print(prob_sentence1)
+
+# prob_sentence = unigram_prob("民") * cprob_brown_2gram["民"].prob("主") * cprob_brown_2gram["主"].prob("國")* cprob_brown_2gram["國"].prob("家")
+# print(prob_sentence) # 0.28532904004829945
